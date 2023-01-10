fc5468baa00fe3b70d077d3b92f70110836c2c15
Tee <sotomi.tayo@gmail.com>
2023-01-10 14:55:44 -0600

    Added validation

--- Number Analyzer/Program.cs
+++ Number Analyzer/Program.cs
@@ -10,34 +10,43 @@ while(Cont == true)
 
     Console.WriteLine(name +" enter an integer between 1 and 100 ");
     int result = int.Parse(Console.ReadLine());
+    
 
-    if (result % 2 != 0 && result < 60)
+    if (result % 2 == 0)
     {
-        Console.WriteLine(result + " Odd and less than 60");
-    }
-    else if (result % 2 == 0 && result >= 2 && result <= 24)
-    {
-        Console.WriteLine("Even and less than 25.");
-    }
-    else if (result % 2 == 0 && result >= 26 && result <= 60)
-    {
-        Console.WriteLine("Even and greater than 60");
-    }
-    else if (result % 2 != 0 && result > 60)
-    {
-        Console.WriteLine("Odd and greater than 60");
-    }
+            Console.WriteLine(name + " your entry is even number");
 
-    Console.WriteLine(name +" would you like to continue? y/n");
-    string Answ = Console.ReadLine();
 
-    if (Answ == "y")
-    {
-        Cont = true;
+            if (result % 2 != 0 && result < 60)
+        {
+            Console.WriteLine(result + " Odd and less than 60");
+        }
+        else if (result % 2 == 0 && result >= 2 && result <= 24)
+        {
+            Console.WriteLine("Even and less than 25.");
+        }
+        else if (result % 2 == 0 && result >= 26 && result <= 60)
+        {
+            Console.WriteLine("Even and between 26 and 60 inclusive");
+        }
+        else if (result % 2 != 0 && result > 60)
+        {
+            Console.WriteLine("Odd and greater than 60");
+        }
+
+        Console.WriteLine(name + " would you like to continue? y/n");
+        string Answ = Console.ReadLine();
+
+        if (Answ == "y")
+        {
+            Cont = true;
+        }
+        else
+        {
+            Cont = false;
+        }
     }
-    else
-    {
-        Cont = false;
-    }
+    else { Console.WriteLine(name + " ,please enter even number"); }
+
 }
 
